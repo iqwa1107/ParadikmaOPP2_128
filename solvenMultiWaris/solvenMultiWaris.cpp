82baaf2484878dfae9135a002f8dfcb70f953f1b
iqwa1107 <ramahs49@gmail.com>
2023-05-29 11:09:56 +0700

    membuat class orang

---
+++ solvenMultiWaris/solvenMultiWaris.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class orang
+{
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "orang di buat dengan umur" << umur << "\n"<< endl;
+
+	}
+
+};
